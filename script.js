215135989ab99b87494bf526b94b27b94a808edf
Jai Sanghvi <85071341+jai-sanghvi@users.noreply.github.com>
2024-11-04 13:17:26 +0530

    Refactor code and add UI

--- script.js
+++ script.js
@@ -1,54 +1,113 @@
-const gridContainer = document.querySelector('.container');
-gridContainer.addEventListener('click', playRound);
+const gameBoard = (function () {
+  const board = ['', '', '', '', '', '', '', '', ''];
+  const winningPatterns = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+  return {board, winningPatterns};
+})();
 
-function makePlayer(name, marker) {
-  return {name, marker};
-}
-
-const player1 = makePlayer("mike", "x");
-const player2 = makePlayer("john", "0");
-
-const gameBoard = [1,2,3,4,5,6,7,8,9];
-const winningPatterns = [[1,4,7],[2,5,8],[3,6,9],[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7]];
-let currentPlayer = (player1.marker === 'x') ? player1 : player2;
-
-function placeMarkerAt(position) {
-  if (gameBoard[position - 1] !== 'x' && gameBoard[position - 1] !== '0') {
-    gameBoard[position - 1] = currentPlayer.marker;
-    
-    const el = document.querySelector(`[data-position="${position}"]`);
-    el.textContent = currentPlayer.marker;
-    // console.table([gameBoard.slice(0,3), gameBoard.slice(3,6), gameBoard.slice(6)]);
-    changeTurns();
+const players = (function () {
+  function makePlayer(name, marker) {
+    return {name, marker};
   }
-}
 
-function changeTurns() {
-  currentPlayer = (currentPlayer === player1) ? player2 : player1;
-}
+  const player1 = makePlayer("Player 1", "x");
+  const player2 = makePlayer("Player 2", "o");
 
-function checkGameStatus() {
-  if (gameBoard.every((el) => el === 'x' || el === '0')) {
-    console.log("it's a tie");
-    return;
-  }
-  for (let pattern of winningPatterns) {
-    let cellsUnderTest = [];
-    for (let pos of pattern) {
-      cellsUnderTest.push(gameBoard[pos - 1]);
+  return [player1, player2];
+})();
+
+const game = (function () {
+  let isStarted = false;
+  let isOver = true;
+  let currentTurn = (players[0].marker === 'x') ? players[0] : players[1];
+  const statusElement = document.querySelector('.status');
+
+  function placeMarker(position, marker) {
+    if (!isOver && isStarted && gameBoard.board[position] === '') {
+      gameBoard.board[position] = marker;
+      displayController.render(position, marker);
+      updateStatus();
+      changeTurns();
     }
-    if (cellsUnderTest.every((el) => el === cellsUnderTest[0])) {
-      console.log(`${(player1.marker === cellsUnderTest[0]) ? player1.name : player2.name} won!`);
+  }
+
+  function updateStatus() {
+    for (let pattern of gameBoard.winningPatterns) {
+      if (gameBoard.board[pattern[0]] !== '' && gameBoard.board[pattern[0]] === gameBoard.board[pattern[1]] && gameBoard.board[pattern[1]] === gameBoard.board[pattern[2]]) {
+        isOver = true;
+        isStarted = false;
+        for (let pos of pattern) {
+          const el = document.querySelector(`[data-position="${pos}"`);
+          el.style.color = '#00ff00';
+        }
+        const winner = (players[0].marker === gameBoard.board[pattern[0]]) ? players[0].name : players[1].name;
+        announceResults('over', winner);
+        return;
+      }
+    }
+    
+    if (gameBoard.board.every((box) => box === 'x' || box === 'o')) {
+      isOver = true;
+      isStarted = false;
+      announceResults('tie');
       return;
     }
   }
-}
 
-function playRound(e) {
+  function getCurrentMarker(currentTurn) {
+    return currentTurn.marker;
+  }
+
+  function changeTurns() {
+    if (!isOver) {
+      game.currentTurn = (game.currentTurn === players[0]) ? players[1] : players[0];
+    }
+  }
+
+  function initialize() {
+    isStarted = true;
+    isOver = false;
+    gameBoard.board.fill('');
+    container.classList.remove("blur");
+    displayController.resetBoard();
+    startButton.textContent = 'Reset Game';
+    statusElement.textContent = '';
+    game.currentTurn = players[0];
+    Array.from(container.children).forEach(child => child.removeAttribute('style'));
+  }
+
+  function announceResults(status, winner) {
+    const str = (status === 'tie') ? "It's a tie!" : `${winner} wins!`;
+    statusElement.textContent = str;
+  }
+
+  return {placeMarker, initialize, getCurrentMarker, currentTurn};
+})();
+
+const displayController = (function () {
+  function render(position, marker) {
+    const el = document.querySelector(`[data-position="${position}"`);
+    el.textContent = marker;
+  }
+
+  function resetBoard() {
+    const els = document.querySelectorAll('[data-position]');
+    els.forEach(el => el.textContent = '');
+  }
+
+  return {render, resetBoard};
+})();
+
+// event listeners
+
+const container = document.querySelector('.container');
+container.addEventListener('click', (e) => {
   if (e.target === e.currentTarget) {
     return;
+  } else {
+    const position = Number(e.target.dataset.position);
+    game.placeMarker(position, game.getCurrentMarker(game.currentTurn));
   }
-  const position = e.target.dataset.position;
-  placeMarkerAt(position);
-  checkGameStatus();
-}
+});
+
+const startButton = document.querySelector('#start-button');
+startButton.addEventListener('click', game.initialize);
